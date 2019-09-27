2634b1ec46d7ac2e5fa0992ec78f58701b2b20fa
Osei <osiqua@gmail.com>
2019-09-26 23:02:40 -0700

    cleaned up html for week 1

--- views/v_group-admin.php
+++ views/v_group-admin.php
@@ -3,122 +3,106 @@
 <main id="admin-page" class="container text-center">
 	<h1>[Group Name] Admin Input</h1>
 
-	<div>
-		<div class="week1">
-			<div class="table-header">
-				<p>Week 1</p>
-				<p class="date">9/23</p>
-				<p class="date">9/24</p>
-				<p class="date">9/25</p>
-				<p class="date">9/26</p>
-				<p class="date">9/27</p>
-				<p class="date">9/28</p>
-				<p>Total</p>
-				<p>Gospel Points</p>
-			</div>
-		</div>
+	<table id="week1">
+		<tr class="table-header">
+			<th>Week 1</th>
+			<th class="date">9/23</th>
+			<th class="date">9/24</th>
+			<th class="date">9/25</th>
+			<th class="date">9/26</th>
+			<th class="date">9/27</th>
+			<th class="date">9/28</th>
+			<th>Total</th>
+			<th>Gospel Points</th>
+		</tr>
 
-		<table id="week1">
-			<tr class="table-header">
-				<th>Week 1</th>
-				<th class="date">9/23</th>
-				<th class="date">9/24</th>
-				<th class="date">9/25</th>
-				<th class="date">9/26</th>
-				<th class="date">9/27</th>
-				<th class="date">9/28</th>
-				<th>Total</th>
-				<th>Gospel Points</th>
-			</tr>
+		<tr>
+			<td class="contents">Connected</td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td class="week-total"></td>
+			<td class="week-points"></td>
+		</tr>
 
-			<tr>
-				<td class="contents">Connected</td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td class="week-total"></td>
-				<td class="week-points"></td>
-			</tr>
+		<tr>
+			<td class="contents">Unconnected</td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td class="week-total"></td>
+			<td class="week-points"></td>
+		</tr>
 
-			<tr>
-				<td class="contents">Unconnected</td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td class="week-total"></td>
-				<td class="week-points"></td>
-			</tr>
+		<tr>
+			<td class="contents">Come & See</td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td class="week-total"></td>
+			<td class="week-points"></td>
+		</tr>
 
-			<tr>
-				<td class="contents">Come & See</td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td class="week-total"></td>
-				<td class="week-points"></td>
-			</tr>
+		<tr>
+			<td class="contents">Baptism Count</td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td class="week-total"></td>
+			<td class="week-points"></td>
+		</tr>
 
-			<tr>
-				<td class="contents">Baptism Count</td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td class="week-total"></td>
-				<td class="week-points"></td>
-			</tr>
+		<tr>
+			<td class="contents">Bible Studies</td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td class="week-total"></td>
+			<td class="week-points"></td>
+		</tr>
 
-			<tr>
-				<td class="contents">Bible Studies</td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td class="week-total"></td>
-				<td class="week-points"></td>
-			</tr>
+		<tr>
+			<td class="contents">EA Confirmations</td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td class="week-total"></td>
+			<td class="week-points"></td>
+		</tr>
 
-			<tr>
-				<td class="contents">EA Confirmations</td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td class="week-total"></td>
-				<td class="week-points"></td>
-			</tr>
-
-			<tr>
-				<td class="contents">MS Confirmations</td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td><input type="number" name=""></td>
-				<td class="week-total"></td>
-				<td class="week-points"></td>
-			</tr>
-		</table>
+		<tr>
+			<td class="contents">MS Confirmations</td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td><input type="number" name=""></td>
+			<td class="week-total"></td>
+			<td class="week-points"></td>
+		</tr>
+	</table>
 
 		<h1>make space</h1>
 		<h1>make space</h1>
-	</div>
 
 </main>
 
