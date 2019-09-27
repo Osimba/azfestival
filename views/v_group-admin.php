6efabdfe1b4b8758ae1e152ab76222006291d16c
Osei <osiqua@gmail.com>
2019-09-26 23:00:34 -0700

    created week 1 table

--- views/v_group-admin.php
+++ views/v_group-admin.php
@@ -18,6 +18,106 @@
 			</div>
 		</div>
 
+		<table id="week1">
+			<tr class="table-header">
+				<th>Week 1</th>
+				<th class="date">9/23</th>
+				<th class="date">9/24</th>
+				<th class="date">9/25</th>
+				<th class="date">9/26</th>
+				<th class="date">9/27</th>
+				<th class="date">9/28</th>
+				<th>Total</th>
+				<th>Gospel Points</th>
+			</tr>
+
+			<tr>
+				<td class="contents">Connected</td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td class="week-total"></td>
+				<td class="week-points"></td>
+			</tr>
+
+			<tr>
+				<td class="contents">Unconnected</td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td class="week-total"></td>
+				<td class="week-points"></td>
+			</tr>
+
+			<tr>
+				<td class="contents">Come & See</td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td class="week-total"></td>
+				<td class="week-points"></td>
+			</tr>
+
+			<tr>
+				<td class="contents">Baptism Count</td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td class="week-total"></td>
+				<td class="week-points"></td>
+			</tr>
+
+			<tr>
+				<td class="contents">Bible Studies</td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td class="week-total"></td>
+				<td class="week-points"></td>
+			</tr>
+
+			<tr>
+				<td class="contents">EA Confirmations</td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td class="week-total"></td>
+				<td class="week-points"></td>
+			</tr>
+
+			<tr>
+				<td class="contents">MS Confirmations</td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td><input type="number" name=""></td>
+				<td class="week-total"></td>
+				<td class="week-points"></td>
+			</tr>
+		</table>
+
+		<h1>make space</h1>
+		<h1>make space</h1>
 	</div>
 
 </main>
